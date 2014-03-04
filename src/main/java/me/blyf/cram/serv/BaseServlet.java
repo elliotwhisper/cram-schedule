bbe35fdcd28462e1990e6e71076c574da9822087
blyfdev@gmail.com <blyfdev@gmail.com>
2014-03-05 06:13:24 +0800

    test

--- src/main/java/me/blyf/cram/serv/BaseServlet.java
+++ src/main/java/me/blyf/cram/serv/BaseServlet.java
@@ -1,18 +1,22 @@
 package me.blyf.cram.serv;
 
-import javax.servlet.ServletException;
+import java.io.StringWriter;
+
 import javax.servlet.http.HttpServlet;
 
+import me.blyf.cram.util.Const;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 
-
-public class BaseServlet extends HttpServlet{
-    private static final long serialVersionUID = -5494369847253378008L;
-    
-    @Override
-    public void init() throws ServletException {
-        String prop = BaseServlet.class.getClassLoader().getResource("velocity.properties").getFile();
-        Velocity.init(prop);
+public abstract class BaseServlet extends HttpServlet{
+	private static final long serialVersionUID = 5361970252965740279L;
+	
+	protected byte[] getByteArray(String template, VelocityContext context){
+    	StringWriter sw = new StringWriter();
+        Template t = Velocity.getTemplate(template);
+        t.merge(context, sw);
+        return sw.toString().getBytes(Const.ENCODING);
     }
-    
 }
--- src/main/java/me/blyf/cram/serv/Service.java
+++ src/main/java/me/blyf/cram/serv/Service.java
@@ -1,18 +1,8 @@
 package me.blyf.cram.serv;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.apache.velocity.Template;
-import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 
 public class Service extends HttpServlet{
@@ -20,32 +10,8 @@ public class Service extends HttpServlet{
 
     @Override
     public void init() throws ServletException {
-        String prop = BaseServlet.class.getClassLoader().getResource("velocity.properties").getFile();
+        String prop = Service.class.getClassLoader().getResource("velocity.properties").getFile();
         Velocity.init(prop);
-        
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
-            System.out.println(sdf.format(new Date(1402761600000L)));
-            System.out.println(new Date().getTime());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doPost(req, resp);
-    }
-    
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        VelocityContext context = new VelocityContext();
-        context.put("name", "elliot");
-        StringWriter sw = new StringWriter();
-        Template template = Velocity.getTemplate("vm/index.vm", "utf8");
-        template.merge(context, sw);
-        resp.getOutputStream().write(sw.toString().getBytes());
-        System.out.println(req.getRequestURL().toString());
-        System.out.println(req.getServletPath());
+        System.out.println("a");
     }
 }
